15a0072291cf02c48a2991d09dee2d66bfa2d8c5
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2022-06-26 23:06:22 +0200

    Id

--- Assets/NodeTransition.cs
+++ Assets/NodeTransition.cs
@@ -1,9 +1,12 @@
 using System;
 using UnityEngine;
 
+[System.Serializable]
 public class NodeTransition : MonoBehaviour, Completeable
 {
     public Action onComplete;
+    [SerializeField] private string m_ID = Guid.NewGuid().ToString();
+    public string ID => m_ID;
 
     public void Complete()
     {
